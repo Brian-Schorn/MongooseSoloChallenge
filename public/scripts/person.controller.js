1f5e36844629fb5ec6d4ca1b3e635378fefbaada
Brian Schorn <schorn.brian@gmail.com>
2017-02-08 09:20:37 -0600

    Added get functionality

--- public/scripts/person.controller.js
+++ public/scripts/person.controller.js
@@ -1,3 +1,7 @@
 app.controller('PersonController', function (PersonService) {
+  var ctrl = this;
 
+  PersonService.getList().then(function(list){
+    console.log(list);
+  });
 });
--- public/scripts/person.service.js
+++ public/scripts/person.service.js
@@ -1,3 +1,30 @@
 app.service('PersonService', function ($http) {
 
+  //get
+this.getList = function () {
+  return $http.get('/person').then(function (response) {
+    console.log("Got a response from the DB", response);
+    return response.data;
+  }).catch(function(err) {
+    console.log('Error getting info from DB', err);
+  });
+};
+
+  //post
+this.addPerson = function () {
+
+}
+
+  //put
+
+
+
+  //delete
+
+
+
+
+
+
+
 });
